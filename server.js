3b70662b5e3e2d009964b7aa5fc15ae3f8e79cb1
chasprowebdev <chasgarciaprowebdev@gmail.com>
2020-10-12 05:27:17 -0400

    update setting for production to heroku

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  res.json(customers);
-});
+// app.get('/api/customers', (req, res) => {
+//   const customers = [
+//     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+//   ];
+
+//   res.json(customers);
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const port = 5000;
 
