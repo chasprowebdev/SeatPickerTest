a524fde6d885be1855f610abfeaf1f9a176c4ef4
chasprowebdev <chasgarciaprowebdev@gmail.com>
2020-10-12 05:55:52 -0400

    remove unused part

--- server.js
+++ server.js
@@ -7,16 +7,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/api/customers', (req, res) => {
-//   const customers = [
-//     {id: 1, firstName: 'John', lastName: 'Doe'},
-//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-//   ];
-
-//   res.json(customers);
-// });
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
